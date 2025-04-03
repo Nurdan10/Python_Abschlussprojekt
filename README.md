bf9e53346b0f087605284bf952d9c2031dd9f381
Nurdan10 <159007457+Nurdan10@users.noreply.github.com>
2025-04-03 23:45:21 +0200

    unnecessary files deleted

--- README.md
+++ README.md
@@ -4,10 +4,16 @@ Projektarbeit: Programmierung mit Python
 
 Projekt: GUI mit Tkinter und Analyse des Census Income Datasets
 
-Beschreibung
+Beschreibung:
 In diesem Projekt habe ich eine grafische Benutzeroberfläche (GUI) mit Tkinter erstellt, um das Census Income Dataset zu analysieren. Das Ziel des Projekts ist es, ein System zu entwickeln, das es dem Benutzer ermöglicht, die Einkommensdaten von Einzelpersonen zu visualisieren und zu analysieren, insbesondere im Hinblick darauf, ob eine Person mehr als 50.000 Dollar jährlich verdient oder nicht.
 
-Dataset
+Warum wurde dieses Projekt gewählt? Welches Problem soll gelöst werden?​
+
+Das Projekt wurde gewählt, um eine benutzerfreundliche grafische Benutzeroberfläche (GUI) zu entwickeln, die es ermöglicht, den Census Income-Datensatz zu analysieren und dabei wichtige demografische Muster und Zusammenhänge zu identifizieren. Das Hauptziel war es, eine Anwendung zu erstellen, die sowohl technisch als auch visuell ansprechend ist, um Benutzern zu helfen, wichtige Informationen über Einkommensverteilungen und deren Abhängigkeit von verschiedenen demografischen Merkmalen zu verstehen.​
+
+Das zu lösende Problem war, eine einfache und interaktive Möglichkeit zu bieten, große und komplexe Datensätze zu analysieren, ohne dass tiefgehende technische Kenntnisse erforderlich sind. Insbesondere wollten wir herausfinden, wie sich Einkommen nach Faktoren wie Bildung, Alter oder Beruf differenziert und ob bestimmte demografische Merkmale signifikante Einkommensunterschiede aufweisen.
+
+Dataset:
 Den verwendeten Datensatz finden Sie hier: https://archive.ics.uci.edu/dataset/2/adult
 Das verwendete Census Income Dataset enthält 48.842 Einträge. Jeder Eintrag enthält Informationen über eine Person, einschließlich ihres Einkommens (ob sie mehr als 50.000 Dollar jährlich verdienen oder nicht), ihrem Alter, Beruf, Bildungsniveau und vielen weiteren demografischen Merkmalen.
 
@@ -41,5 +47,20 @@ capital-loss: Kapitalverluste einer Person.
 hours-per-week: Die Anzahl der Arbeitsstunden pro Woche.
 
 native-country: Das Herkunftsland der Person.
+
+Prozess des Projekts:
+- Vor Beginn des Projekts wurde ein Repository namens "python_Abschlussprojekt" auf GitHub erstellt.
+- Mit VS Code auf meinen Computer geklont. 
+- Die gewünschte CSV-Datei wurde hochgeladen. 
+- Eine virtuelle Umgebung wurde eingerichtet
+- Die benötigten Python-Bibliotheken wurden installiert. 
+- Im Ordner wurde eine Python-Datei erstellt, in der der Code entwickelt wurde. 
+- Die Arbeit begann mit der __init__-Methode einer Klasse. 
+- Es wurden mehrere Klassen verwendet: class PlotWindow, class PlotHandler, class MessageBoxHandler. 
+- Unter diesen Klassen wurden Funktionen definiert, wie Navigator, Buttons, Plot-Erstellung und -Speicherung sowie Funktionen für das Arbeiten mit Bildern und Text. 
+- Zum Schluss funktioniert die GUI  erfolgreich und es ist sicherlich möglich, in der Zukunft Verbesserungen vorzunehmen.
+
+
+-----------------Vielen Dank!---------------------------------
 
 
--- US_citizens_pie_subplot.py
+++ US_citizens_pie_subplot.py
@@ -9,13 +9,19 @@ Funktionalitäten:
 - Speicherung und Anzeige der generierten Diagramme in einem Webbrowser.
 
 Technische Umsetzung:
-- Tkinter für die GUI.
+- Tkinter für die Haupt-GUI und `ttk` für die stilisierten Widgets.
 - Pandas für die CSV-Verarbeitung.
-- Plotly für die Diagrammerstellung.
-- Webbrowser-Modul zur Anzeige von Diagrammen.
+- Plotly für die Diagrammerstellung:
+    plotly.express für schnelle und interaktive Diagramme,
+    plotly.graph_objects für komplexe und anpassbare Diagramme,
+    plotly.subplots für das Erstellen von Subplots.
+- Webbrowser-Modul zur Anzeige von Diagrammen: webbrowser zum Öffnen von Diagrammen im Standard-Webbrowser.
+- Pillow für die Bildbearbeitung:
+    PIL.ImageTk für die Integration von Bildern in Tkinter, 
+    PIL.ImageDraw und PIL.ImageFont für das Hinzufügen von Text auf Bildern.
 
-Autor: [Dein Name]
-Datum: [Aktuelles Datum]
+Autor: Nurdan Cakir
+Datum: 04.04.2025
 """
 
 
@@ -31,7 +37,9 @@ import plotly.graph_objects as go
 import webbrowser
 
 def mapping_education(x):
-    """Weist jeder Bildungsstufe eine allgemeine Kategorie zu."""
+    """
+    Weist jeder Bildungsstufe eine allgemeine Kategorie zu.
+    """
     if x in ["Preschool", "1st-4th", "5th-6th", "7th-8th", "9th", "10th", "11th", "12th"]:
         return "low_level_grade"
     elif x in ["HS-grad", "Some-college", "Assoc-voc", "Assoc-acdm"]:
@@ -41,13 +49,18 @@ def mapping_education(x):
     return None
 
 def mapping_marital_status(x):
+    """
+    Die Funktion weist dem Familienstand eine allgemeine Kategorie zu:
+    """
     if x in ["Never-married", "Divorced", "Separated", "Widowed"]:
         return "unmarried"
     elif x in ["Married-civ-spouse", "Married-AF-spouse", "Married-spouse-absent"]:
         return "married"
 
 def setup_styles():
-    """Konfiguriert das visuelle Erscheinungsbild der GUI mit Tkinter Style."""
+    """
+    Konfiguriert das visuelle Erscheinungsbild der GUI mit Tkinter Style.
+    """
     style = ttk.Style()
     style.configure("TLabel", font=("Arial", 12, "bold"), foreground="#1565c0", background="#e3f2fd")
     style.configure("Soft.TButton", padding=6, relief="flat", background="#a0c4ff", font=("Arial", 12, "bold"))
@@ -72,7 +85,14 @@ class PlotWindow:
 
         # Navigation Frame für Buttons und Optionen
         self.nav_frame = tk.Frame(self.pane, width=250, bg="#e3f2fd")
+        self.nav_frame.pack_propagate(False)  # İçeriğin boyutlandırmasını engeller
         self.pane.add(self.nav_frame)
+    
+        # Erstellen eines zusätzlichen Rahmens für das Hinzufügen 
+        # von Padding an der oberen Seite des Navigationsrahmens.
+
+        top_frame = tk.Frame(self.nav_frame, bg="#e3f2fd", height=100)  # 50px padding için bir frame
+        top_frame.pack(fill=tk.X)
         
         # Visualisierungsbereich
         self.vis_frame = tk.Frame(self.pane, width=600, bg="white")
@@ -80,36 +100,23 @@ class PlotWindow:
 
         self.add_image_with_text()
         
-
     def add_image_with_text(self):
-        """Resmin üzerine uygulama adı veya mesaj ekleyerek ekrana getirir."""
-        img_path = "your_image_path.jpg"  # Resmin yolunu buraya yazın
+        """
+        Das Hinzufügen von Anwendungstitel oder Nachricht auf das Bild zur Anzeige auf dem Bildschirm.
+
+        """
+        img_path = "C:/Users/nurda/Python_Abschlussprojekt/images/data_visualization.jpg"  # Resmin yolunu buraya yazın
         img = Image.open(img_path)
-        img = img.resize((600, 400), Image.ANTIALIAS)
-        
-        # Üzerine yazı eklemek için çizim başlat
-        draw = ImageDraw.Draw(img)
-
-        # Yazı tipi ayarla (Varsayılan font kullanabiliriz)
-        try:
-            font = ImageFont.truetype("arial.ttf", 40)  # Sisteminizde Arial yoksa farklı bir font kullanın
-        except:
-            font = ImageFont.load_default()
-
-        text = "Welcome to DataViz App"  # Buraya istediğiniz metni yazabilirsiniz
-        text_width, text_height = draw.textsize(text, font=font)
-
-        # Yazıyı merkeze yerleştir
-        text_x = (img.width - text_width) // 2
-        text_y = 20  # Üstten biraz boşluk bırak
-
-        # Yazıyı çiz (Beyaz arka plan, siyah font)
-        draw.text((text_x, text_y), text, font=font, fill="white")
-
-        # Tkinter ile göstermek için dönüştür
+        img = img.resize((400, 400), Image.Resampling.LANCZOS)  # Resmin boyutunu büyütüyoruz
+    
+        # Der Beginn des Zeichnens zum Hinzufügen von Text.
         img_tk = ImageTk.PhotoImage(img)
-        
-        # Resmi bir Label içinde göster
+
+        text = "Welcome to DataViz App"
+        label_text = Label(self.vis_frame, text=text, font=("Arial", 24, "bold"), bg="white", fg="black")
+        label_text.pack(pady=(20, 10))
+    
+        # Zeigt das Bild in einem Label
         self.image_label = Label(self.vis_frame, image=img_tk)
         self.image_label.image = img_tk  # Referansı kaybetmemek için
         self.image_label.pack(padx=10, pady=10)
@@ -118,97 +125,112 @@ class PlotWindow:
 
     def create_widgets(self):
         """Erstellt die Widgets zur Steuerung der Anwendung."""
+        # Liste der verfügbaren Diagrammtypen
         self.plot_types = ["Bar", "Pie", "Histogram", "Line", "Box", "Scatter"]
+        # Standardmäßig ausgewählter Diagrammtyp
         self.selected_plot = tk.StringVar(value=self.plot_types[0])
-
+        
+        # Label für die Auswahl des Diagrammtyps
         ttk.Label(self.nav_frame, text="Select Plot Type:", style="TLabel").pack(pady=5)
+        
+        # Dropdown-Menü für die Auswahl des Diagrammtyps
         self.plot_dropdown = ttk.Combobox(self.nav_frame, textvariable=self.selected_plot, values=self.plot_types)
         self.plot_dropdown.pack(pady=5)
+        # Ereignisbindung: Wenn der Benutzer einen Diagrammtyp auswählt, wird die Spaltenauswahl aktualisiert
         self.plot_dropdown.bind("<<ComboboxSelected>>", self.update_column_dropdown)
         
+        # Liste der Spalten aus dem DataFrame
         self.all_columns = self.df.columns.tolist()
+        # Standardmäßig ausgewählte Spalte für die erste und zweite Auswahl
         self.selected_col1 = tk.StringVar(value=self.all_columns[0])
         self.selected_col2 = tk.StringVar(value=self.all_columns[0])
         
+        # Label für die Auswahl der ersten Spalte
         ttk.Label(self.nav_frame, text="Select First Column:", style="TLabel").pack(pady=5)
+        # Dropdown-Menü für die Auswahl der ersten Spalte
         self.col1_dropdown = ttk.Combobox(self.nav_frame, textvariable=self.selected_col1, values=self.all_columns)
         self.col1_dropdown.pack(pady=5)
         
+        # Label für die Auswahl der zweiten Spalte
         ttk.Label(self.nav_frame, text="Select Second Column:", style="TLabel").pack(pady=5)
+        # Dropdown-Menü für die Auswahl der zweiten Spalte
         self.col2_dropdown = ttk.Combobox(self.nav_frame, textvariable=self.selected_col2, values=self.all_columns)
         self.col2_dropdown.pack(pady=5)
         
+        # Button zum Erstellen des Diagramms
         self.plot_button = ttk.Button(self.nav_frame, text="Plot", style="Soft.TButton", command=self.plot_graph)
         self.plot_button.pack(pady=5)
         
+        # Button für eine Informationsnachricht
         self.info_button = ttk.Button(self.nav_frame, text="Info", style="Soft.TButton", command=self.show_message)
         self.info_button.pack(pady=5)
         
+        # Button zum Beenden der Anwendung
         self.quit_button = ttk.Button(self.nav_frame, text="Quit", style="Soft.TButton", command=self.root.quit)
         self.quit_button.pack(pady=5)
 
-    """def plot_pie_chart(self):
-        col1 = self.col1_var.get()
-        col2 = self.col2_var.get()
-        
-        if col1 not in self.df.columns or col2 not in self.df.columns:
-            messagebox.showerror("Error", "Selected columns not found!")
-            return
-
-        unique_values = self.df[col1].unique()
-        num_plots = min(len(unique_values), 6)
-        
-        fig = make_subplots(rows=1, cols=num_plots, subplot_titles=[str(val) for val in unique_values[:num_plots]], specs=[[{'type':'domain'}]*num_plots])
-        
-        for i, value in enumerate(unique_values[:num_plots]):
-            subset = self.df[self.df[col1] == value][col2].value_counts()
-            fig.add_trace(go.Pie(labels=subset.index, values=subset.values, name=str(value)), row=1, col=i+1)
-        
-        fig.update_layout(title_text=f"Pie Chart Subplots of {col1} by {col2}")
-        fig.show()"""
-
     def update_column_dropdown(self, event=None):
         """Aktualisiert die Drop-down-Menüs mit den CSV-Spaltennamen."""
+        # Der aktuell ausgewählte Diagrammtyp wird ermittelt
         plot_type = self.selected_plot.get()
 
+        # Wenn der Diagrammtyp "Histogram" ist, werden nur die erste Spalte ausgeführt und die zweite Dropdown-Liste wird auf "---" gesetzt
         if plot_type == "Histogram":
             self.col2_dropdown.config(values=["---"])
+            # Wenn der Diagrammtyp "Bar" oder "Pie" ist, wird nur mit Spalten vom Datentyp 'object' 
+            # (kategorische Spalten) gearbeitet
         elif plot_type in ["Bar", "Pie"]:
+            # Die Spalten mit dem Datentyp 'object' (kategorische Daten) werden als Werte 
+            # für die Dropdown-Menüs verwendet
             self.col1_dropdown.config(values=self.df.select_dtypes(include=['object']).columns.tolist())
+            # Für die zweite Spalte wird zusätzlich ein "---" als Auswahloption hinzugefügt
             self.col2_dropdown.config(values=self.df.select_dtypes(include=['object']).columns.tolist() + ["---"])
+            # Für alle anderen Diagrammtypen werden alle Spalten des DataFrames als Optionen 
+            # für die Dropdown-Listen angeboten
         else:
             self.col1_dropdown.config(values=self.df.columns.tolist())
+            # Für die zweite Spalte wird ebenfalls ein "---" als Auswahloption hinzugefügt
             self.col2_dropdown.config(values=self.df.columns.tolist() + ["---"])
+
 
     def plot_graph(self):
         """Erstellt ein Diagramm basierend auf den Benutzereinstellungen."""
+        # Der ausgewählte Diagrammtyp und die ausgewählten Spalten werden abgerufen
         plot_type = self.selected_plot.get()
         col1 = self.selected_col1.get()
         col2 = self.selected_col2.get()
 
-        # Erzeugung des Plots entsprechend dem gewählten Typ
+        # Überprüfen der Eingaben basierend auf dem ausgewählten Diagrammtyp und den Daten
+        # Überprüfung für Balkendiagramme: Beide Spalten sollten kategorisch sein
         if plot_type == "Bar" and not (self.df[col1].dtype == 'object' and (self.df[col2].dtype == 'object' or col2 == "---")):
             messagebox.showwarning("Warning", "For Bar charts, both columns should be categorical!")
             return
+        # Überprüfung für Tortendiagramme: Die erste Spalte sollte kategorisch sein
         elif plot_type == "Pie" and not (self.df[col1].dtype == 'object'):
             messagebox.showwarning("Warning", "For Pie charts, the first column should be categorical!")
             return
+        # Überprüfung für Histogramme: Die erste Spalte sollte numerisch sein
         elif plot_type == "Histogram" and not (self.df[col1].dtype in ['float64', 'int64']):
             messagebox.showwarning("Warning", "For Histogram, the first column should be numerical!")
             return
+        # Überprüfung für Liniendiagramme: Beide Spalten sollten numerisch sein
         elif plot_type == "Line" and not (self.df[col1].dtype in ['float64', 'int64'] and self.df[col2].dtype in ['float64', 'int64']):
             messagebox.showwarning("Warning", "For Line charts, both columns should be numerical!")
             return
+        # Überprüfung für Streudiagramme: Beide Spalten sollten numerisch sein
         elif plot_type == "Scatter" and not (self.df[col1].dtype in ['float64', 'int64'] and self.df[col2].dtype in ['float64', 'int64']):
             messagebox.showwarning("Warning", "For Scatter plots, both columns should be numerical!")
             return
+        # Überprüfung für Boxplots: Die zweite Spalte sollte numerisch sein
         elif plot_type == "Box" and not (self.df[col2].dtype in ['float64', 'int64']):
             messagebox.showwarning("Warning", "For Box plots, the second column should be numerical!")
             return
 
+        # Warnung, wenn die Anzahl der einzigartigen Werte zu hoch ist für Balken- oder Tortendiagramme
         if self.df[col1].nunique() > 50 and plot_type in ["Bar", "Pie"]:
             messagebox.showwarning("Warning", "The selected plot type may not be suitable due to too many unique values!")
 
+        # Erstellen und Initialisieren des PlotHandlers entsprechend dem Diagrammtyp
         if plot_type == "Bar":
             self.plot_handler = PlotHandler(self.df, "bar", col1, col2)
         elif plot_type == "Pie":
@@ -225,10 +247,13 @@ class PlotWindow:
             messagebox.showerror("Error", "Invalid plot type selected!")
             return
 
+        # Erstellen des Diagramms
         self.plot_handler.generate_plot()
 
+        # Anzeige einer Informationsnachricht
     def show_message(self):
         MessageBoxHandler(self.df)
+
 
 
 class PlotHandler:
@@ -252,17 +277,36 @@ class PlotHandler:
         Erstellt ein Diagramm basierend auf den ausgewählten Parametern und zeigt es an.
     """
     def __init__(self, df, plot_type, col1, col2):
+        """
+        Initialisiert den PlotHandler mit den übergebenen Parametern.
+        """
+        # DataFrame mit den CSV-Daten
         self.df = df
+        # Der gewählte Diagrammtyp (z.B. 'bar', 'pie', 'histogram', etc.)
         self.plot_type = plot_type
+        # Die erste Spalte für das Diagramm
         self.col1 = col1
+        # Die zweite Spalte für das Diagramm (oder "---", falls nicht benötigt)
         self.col2 = col2
 
     def generate_plot(self):
+        """
+        Erstellt und zeigt ein Diagramm basierend auf den ausgewählten Parametern.
+
+        Der Diagrammtyp wird basierend auf der Benutzerauswahl erstellt. Es werden 
+        verschiedene Diagrammtypen unterstützt, einschließlich Bar, Pie, Histogramm,
+        Line, Box und Scatter. Alle Diagramme erhalten einen Titel und eine 
+        angepasste Formatierung.
+
+        Wenn der Diagrammtyp oder die Spaltenkombination nicht geeignet ist, wird
+        eine Fehlermeldung angezeigt.
+        """
         plot_title = f"{self.plot_type} plot of {self.col1} and {self.col2}".title()
         title_style = dict(font=dict(size=20, color="blue", family="Arial", weight="bold"))
 
+        # Erstellen eines Bar-Diagramms
         if self.plot_type == "bar":
-            if self.col2 == "---":  # Wenn nicht zeite Spalte is ausgewält
+            if self.col2 == "---":  # Wenn keine zweite Spalte ausgewählt ist
                 count_df = self.df[self.col1].value_counts().reset_index(name="Count")
                 fig = px.bar(count_df, x="index", y="Count")
             else:
@@ -270,9 +314,10 @@ class PlotHandler:
                 fig = px.bar(count_df, x=self.col1, y="Count", color=self.col2, barmode="group")
             fig.update_layout(title=plot_title, title_font=dict(size=20, color="blue", family="Arial", weight="bold"))
 
+        # Erstellen eines Pie-Diagramms
         elif self.plot_type == "pie":
             # We handle only the subplot version of the pie chart
-            if self.col1 == "salary":  # group by salary
+            if self.col1 == "salary":  # Gruppierung nach Gehalt
                 fig = make_subplots(rows=1, cols=2, subplot_titles=("<=50K", ">50K"), specs=[[{"type": "domain"}, {"type": "domain"}]])
             
                 for i, salary_group in enumerate(["<=50K", ">50K"]):
@@ -287,22 +332,27 @@ class PlotHandler:
                 fig.update_layout(title=plot_title, title_font=dict(size=20, color="blue", family="Arial", weight="bold"))
                 fig.show()
 
+        # Erstellen eines Histogramms
         elif self.plot_type == "histogram":
             fig = px.histogram(self.df, x=self.col1, color=self.col2 if self.col2 != "---" else None)
             fig.update_layout(title=plot_title, title_font=dict(size=20, color="blue", family="Arial", weight="bold"))
 
+        # Erstellen eines Liniendiagramms
         elif self.plot_type == "line":
             fig = px.line(self.df, x=self.col1, y=self.col2)
             fig.update_layout(title=plot_title, title_font=dict(size=20, color="blue", family="Arial", weight="bold"))
 
+        # Erstellen eines Box-Diagramms
         elif self.plot_type == "box":
             fig = px.box(self.df, x=self.col1, y=self.col2)
             fig.update_layout(title=plot_title, title_font=dict(size=20, color="blue", family="Arial", weight="bold"))
 
+        # Erstellen eines Scatter-Diagramms
         elif self.plot_type == "scatter":
             fig = px.scatter(self.df, x=self.col1, y=self.col2)
             fig.update_layout(title=plot_title, title_font=dict(size=20, color="blue", family="Arial", weight="bold"))
 
+         # Fehlerbehandlung bei ungültigem Diagrammtyp
         else:
             messagebox.showerror("Error", "Invalid plot type selected!")
             return
@@ -312,14 +362,19 @@ class PlotHandler:
         webbrowser.open("plot.html")
 
         # Speicherung des Plots
+        # Abfrage, ob der Benutzer das Diagramm speichern möchte
         save_plot = messagebox.askyesno("Save Plot", "Do you want to save this plot?")
         
         if save_plot:
+             # Optionen für Dateitypen (PNG oder PDF)
             filetypes = [("PNG file", "*.png"), ("PDF file", "*.pdf")]
+            # Dialog zur Dateiauswahl und -speicherung
             file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=filetypes)
             
             if file_path:
+                # Das Diagramm im gewählten Dateiformat speichern
                 fig.write_image(file_path)
+                # Erfolgsmeldung anzeigen
                 messagebox.showinfo("Success", f"Plot saved as {file_path}")
 
         """save_plot = messagebox.askyesno("Save Plot", "Do you want to save this plot?")
@@ -346,17 +401,34 @@ class PlotHandler:
 
 
 class MessageBoxHandler:
+    """
+    Diese Klasse wird verwendet, um eine Informationsbox über das Dataset anzuzeigen.
+    
+    Methoden:
+    ---------
+    show_info(): Zeigt die Informationen des Datensatzes an, einschließlich der Anzahl der Zeilen, Spalten und fehlenden Werte.
+    """
     def __init__(self, df):
+        # Der DataFrame wird als Argument übergeben
         self.df = df
+        # Ruft die Methode zur Anzeige der Datensatzinformationen au
         self.show_info()
 
     def show_info(self):
+        """
+        Zeigt eine Informationsbox mit den grundlegenden Details des DataFrames an:
+        - Anzahl der Zeilen
+        - Anzahl der Spalten
+        - Anzahl der fehlenden Werte
+        """
         info_text = f"Dataset Info:\nRows: {self.df.shape[0]}\nColumns: {self.df.shape[1]}\nMissing Values: {self.df.isnull().sum().sum()}"
+        # Zeigt das Info-Fenster mit den Daten an
         messagebox.showinfo("Dataset Information", info_text)
 
-
+# Der Hauptcode, um die Anwendung zu starten
 if __name__ == "__main__":
-    root = tk.Tk()
-    setup_styles()
+    root = tk.Tk()  # Erstellt das Haupt-Tkinter-Fenster
+    setup_styles()  # Wendet die Styles für die GUI an
+    # Erstellt das PlotWindow mit dem DataFrame aus der CSV-Datei
     app = PlotWindow(root, "adult_eda.csv")
     root.mainloop()
